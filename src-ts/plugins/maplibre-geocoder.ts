8980d88fd2f7233d9115502a3a20a9a6acce7d34
Stefan Kuethe <stefan.kuethe@eoda.de>
2025-04-20 10:14:21 +0200

    Build ipywidget without ml-geocoder.css

--- src-ts/plugins/maplibre-geocoder.ts
+++ src-ts/plugins/maplibre-geocoder.ts
@@ -1,48 +1,50 @@
 import MaplibreGeocoder, {
-  CarmenGeojsonFeature,
-  MaplibreGeocoderApi,
-  MaplibreGeocoderApiConfig,
-  MaplibreGeocoderOptions,
+    CarmenGeojsonFeature,
+    MaplibreGeocoderApi,
+    MaplibreGeocoderApiConfig,
+    MaplibreGeocoderOptions,
 } from "@maplibre/maplibre-gl-geocoder";
-import "@maplibre/maplibre-gl-geocoder/dist/maplibre-gl-geocoder.css";
+
+// Overwrites MapTilerGecodingControl css
+// import "@maplibre/maplibre-gl-geocoder/dist/maplibre-gl-geocoder.css";
 
 export default function gecoder(
-  options: MaplibreGeocoderOptions,
+    options: MaplibreGeocoderOptions,
 ): MaplibreGeocoder {
-  const geocoderApi: any = {
-    forwardGeocode: async (config: MaplibreGeocoderApiConfig) => {
-      const features = [];
-      try {
-        const request = `https://nominatim.openstreetmap.org/search?q=${config.query}&format=geojson&polygon_geojson=1&addressdetails=1`;
-        const response = await fetch(request);
-        const geojson = await response.json();
-        for (const feature of geojson.features) {
-          const center = [
-            feature.bbox[0] + (feature.bbox[2] - feature.bbox[0]) / 2,
-            feature.bbox[1] + (feature.bbox[3] - feature.bbox[1]) / 2,
-          ];
-          const point = {
-            type: "Feature",
-            geometry: {
-              type: "Point",
-              coordinates: center,
-            },
-            place_name: feature.properties.display_name,
-            properties: feature.properties,
-            text: feature.properties.display_name,
-            place_type: ["place"],
-            center,
-          };
-          features.push(point);
-        }
-      } catch (e) {
-        console.error("forwardGeocode", e);
-      }
+    const geocoderApi: any = {
+        forwardGeocode: async (config: MaplibreGeocoderApiConfig) => {
+            const features = [];
+            try {
+                const request = `https://nominatim.openstreetmap.org/search?q=${config.query}&format=geojson&polygon_geojson=1&addressdetails=1`;
+                const response = await fetch(request);
+                const geojson = await response.json();
+                for (const feature of geojson.features) {
+                    const center = [
+                        feature.bbox[0] + (feature.bbox[2] - feature.bbox[0]) / 2,
+                        feature.bbox[1] + (feature.bbox[3] - feature.bbox[1]) / 2,
+                    ];
+                    const point = {
+                        type: "Feature",
+                        geometry: {
+                            type: "Point",
+                            coordinates: center,
+                        },
+                        place_name: feature.properties.display_name,
+                        properties: feature.properties,
+                        text: feature.properties.display_name,
+                        place_type: ["place"],
+                        center,
+                    };
+                    features.push(point);
+                }
+            } catch (e) {
+                console.error("forwardGeocode", e);
+            }
 
-      return {
-        features,
-      };
-    },
-  };
-  return new MaplibreGeocoder(geocoderApi, options);
+            return {
+                features,
+            };
+        },
+    };
+    return new MaplibreGeocoder(geocoderApi, options);
 }
